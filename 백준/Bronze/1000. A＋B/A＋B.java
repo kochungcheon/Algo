c40e53990f5a357e8f2caab377286a48de8cd0e8
kochungcheon <g2c1010@naver.com>
2023-06-19 18:43:10 +0900

    [Bronze V] Title: A+B, Time: 108 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main
+    {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.print(A+B);
+    }
+}
