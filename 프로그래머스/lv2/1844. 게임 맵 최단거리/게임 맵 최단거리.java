0976bf08bbcbb30d500b43c96a7de20b505651e4
kochungcheon <g2c1010@naver.com>
2023-08-28 15:19:53 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.30 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,56 +1,38 @@
+
 import java.util.*;
+
 class Solution {
-    static class Pair{
-        int x, y, step;
-        public Pair(int x, int y, int step){
-            this.x = x;
-            this.y = y;
-            this.step = step;
-        }
-    }
-    static int grid_y;
-    static int grid_x;
-    static int ans = -1;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static Queue<Pair> q = new LinkedList<>();
-    static int[][] visit;
-    static void push(int x, int y, int step){
-        visit[x][y] = step;
-        q.add(new Pair(x, y, step));
-    }
-    static void BFS(int[][] maps){
-        push(0, 0, 1);
-        while (!q.isEmpty()){
-            Pair cur = q.poll();
-            for (int i=0; i<4; i++){
-                int nx = dx[i] + cur.x;
-                int ny = dy[i] + cur.y;
-                if (canGo(nx, ny, maps)){
-                    push(nx, ny, cur.step+1);
+    public int solution(int[][] maps) {
+        int rows = maps.length;
+        int cols = maps[0].length;
+
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상하좌우
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0, 1}); // 시작 위치와 거리
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int row = current[0];
+            int col = current[1];
+            int distance = current[2];
+
+            if (row == rows - 1 && col == cols - 1) {
+                return distance; // 목적지에 도달한 경우 최단거리 반환
+            }
+
+            for (int[] dir : directions) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && maps[newRow][newCol] == 1) {
+                    maps[newRow][newCol] = 0; // 방문한 위치는 재방문하지 않도록 표시
+                    queue.offer(new int[]{newRow, newCol, distance + 1});
                 }
             }
         }
-    }
-    static boolean canGo(int x, int y, int[][] maps){
-        if (!inRage(x, y)) return false;
-        if (visit[x][y] > 0 || maps[x][y] == 0) return false;
-        return true;
-    }
-    static boolean inRage(int x, int y){
-        return 0 <= x && x < grid_x && 0 <= y && y < grid_y;
-    }
-    public int solution(int[][] maps) {
-        grid_x = maps.length;
-        grid_y = maps[0].length;
-        visit = new int[grid_x][grid_y];
-        BFS(maps);
-        // for (int i=0; i<grid_x; i++){
-        //     for (int j=0; j)
-        // }
-        if (visit[grid_x-1][grid_y-1] > 0){
-            ans = visit[grid_x-1][grid_y-1];
-        }
-        return ans;
+
+        return -1; // 목적지에 도달하지 못한 경우
     }
 }
+
