f50b9d3a63bc2eba156584b6813148628babd188
kochungcheon <g2c1010@naver.com>
2023-09-04 11:36:57 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -1,33 +1,24 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int N = progresses.length;
         ArrayList<Integer> arr = new ArrayList<Integer>();
         arr.add(1);
-        
- 
         int compare = (100 - progresses[0] + speeds[0] - 1) / speeds[0];
         int index = 0;
-        
-        for (int i = 1; i < N; i++) {
-
-            int days = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
-            
-            if (compare >= days) {
-                arr.set(index, arr.get(index) + 1);
-            } else {
+        for (int i=1; i<N; i++){
+            if (compare >= (100 - progresses[i] + speeds[i] - 1) / speeds[i]){
+                arr.set(index, arr.get(index)+1);
+            } else{
                 arr.add(1);
                 index++;
-                compare = days;
+                compare = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
             }
         }
-        
         int[] answer = new int[arr.size()];
-        for (int i = 0; i < arr.size(); i++) {
+        for (int i=0; i<arr.size(); i++){
             answer[i] = arr.get(i);
         }
-        
         return answer;
     }
 }
