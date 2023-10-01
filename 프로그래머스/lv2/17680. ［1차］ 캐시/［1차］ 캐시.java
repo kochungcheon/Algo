4ed5abc5024359a6364eafdbd125c49b1b595a3e
kochungcheon <g2c1010@naver.com>
2023-10-02 00:02:52 +0900

    [level 2] Title: [1차] 캐시, Time: 1.94 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,35 +1,33 @@
 import java.util.*;
-
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        if (cacheSize == 0) {
-            return 5 * cities.length;  
-        }
-
-        Queue<String> lru = new LinkedList<>();
-        Set<String> cacheSet = new HashSet<>();
         int answer = 0;
-
-        for (String city : cities) {
-            city = city.toLowerCase();  
-
-            if (cacheSet.contains(city)) {
-              
+        LinkedList<String> lru = new LinkedList<>();
+        Set<String> cash = new HashSet<>();
+        if (cacheSize == 0){
+            return 5 * cities.length;
+        }
+        for (String city : cities){
+            city = city.toLowerCase();
+            if (cash.contains(city)){
                 lru.remove(city);
-                lru.add(city);
+                lru.addLast(city);
                 answer += 1;
-            } else {
-             
-                if (lru.size() == cacheSize) {
-                    String leastRecentlyUsed = lru.poll();  
-                    cacheSet.remove(leastRecentlyUsed);
+            } else{
+                if (lru.size() < cacheSize){
+                    cash.add(city);
+                    lru.addLast(city);
+                    answer += 5;
                 }
-                lru.add(city);
-                cacheSet.add(city);
-                answer += 5;
+                
+                else{
+                    cash.remove(lru.poll());
+                    cash.add(city);
+                    lru.addLast(city);
+                    answer += 5;
+                }
             }
         }
-        
         return answer;
     }
 }
