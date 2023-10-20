dfc490eaca33820efc9ccd06d8f7004ec37322e7
kochungcheon <g2c1010@naver.com>
2023-10-20 19:06:11 +0900

    [level 2] Title: 영어 끝말잇기, Time: 10.61 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -3,29 +3,24 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
-        Set<String> map = new HashSet<String>();
+        Map<String, Integer> map = new HashSet<String, Integer>();
         String pre = "";
         int cnt = 0;
         for (String word : words){
-            // 시작
-            cnt++;
             if (pre.equals("")){
                 pre = word;
-                map.add(word);
+                map.put(word, cnt);
             } else {
-                // 끝말잇기가 가능 한지 체크
-                if (pre.charAt(pre.length()-1) != word.charAt(0) || map.contains(word)){
-                    System.out.println(cnt + " " + n);
-                    answer[0] = (cnt % n) == 0 ? n : cnt % n;
-                    answer[1] = (cnt % n) == 0 ? cnt / n : cnt / n + 1;
-                    break;
+                if (pre.charAt(pre.length()-1) == word.charAt(0)){
+                    pre = word;
+                    map.put(word, cnt);
                 }
-                pre = word;
-                map.add(word);
             }
+            cnt++;
         }
-        
-        
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다. 
+        System.out.println("Hello Java");
+
         return answer;
     }
 }
