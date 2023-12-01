fe4317198807ac8a7e8258a0f5321a9ecb4bac6d
kochungcheon <g2c1010@naver.com>
2023-12-02 00:16:58 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 10.03 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -5,12 +5,12 @@ class Solution {
             if (numbers[i] % 2 == 0) {
                 answer[i] = numbers[i] + 1;
             } else {
-                long number = numbers[i];
+                long num = numbers[i];
                 long mask = 1;
-                while ((number & mask) != 0) {
+                while ((num & mask)!=0) {
                     mask <<= 1;
                 }
-                answer[i] = (number | mask) & ~(mask >> 1);
+                answer[i] = (mask|num) & ~(mask>>1);
             }
         }
         return answer;
