15a4420cd78091e8855706d19dcde224ffaf4bca
kochungcheon <g2c1010@naver.com>
2024-02-18 23:40:58 +0900

    [Bronze V] Title: 자동완성, Time: 108 ms, Memory: 12752 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        System.out.println(new Scanner(System.in).next().equalsIgnoreCase("N") ? "Naver D2" : "Naver Whale");
+    }
+}
