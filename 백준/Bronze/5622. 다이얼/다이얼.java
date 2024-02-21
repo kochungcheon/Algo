f8be268b6d580fe671bec0b004862479566829ea
kochungcheon <g2c1010@naver.com>
2024-02-21 18:15:40 +0900

    [Bronze II] Title: 다이얼, Time: 80 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String s = st.nextToken();
+
+        int[] arr = new int[] {3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10};
+        int ans = 0;
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            ans += arr[c - 'A'];
+        }
+        System.out.println(ans);
+    }
+}
