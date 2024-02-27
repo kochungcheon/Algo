a52de69f95e439deaeb4ee3f24f929df752f0aa4
kochungcheon <g2c1010@naver.com>
2024-02-27 17:59:44 +0900

    [Gold V] Title: 홀수 홀릭 호석, Time: 84 ms, Memory: 12052 KB, Score: 102 point -BaekjoonHub

--- 백준/Gold/20164. 홀수 홀릭 호석/홀수 홀릭 호석.java
+++ 백준/Gold/20164. 홀수 홀릭 호석/홀수 홀릭 호석.java
@@ -5,41 +5,45 @@ import java.io.InputStreamReader;
 public class Main {
     static int minAns = Integer.MAX_VALUE;
     static int maxAns = -1;
-    public static int solve(String n, int ans) {
-        int cnt = 0;
-        for (char c : n.toCharArray()) {
-            if ((c - '0') % 2 == 1) {
-                cnt++;
+    public static int find_cnt(int n) {
+        int tmp = 0;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                tmp++;
             }
+            n /= 10;
         }
-        ans += cnt;
+        return tmp;
+    }
+    public static void solve(int n, int ans) {
+        if (n < 10) {
+            minAns = Math.min(ans, minAns);
+            maxAns = Math.max(ans, maxAns);
+        }
 
-        if (n.length() >= 3) {
-            for (int i = 1; i < n.length() - 1; i++) {
-                for (int j = i + 1; j < n.length(); j++) {
-                    int a = Integer.parseInt(n.substring(0, i));
-                    int b = Integer.parseInt(n.substring(i, j));
-                    int c = Integer.parseInt(n.substring(j));
-                    int newAns = solve(String.valueOf(a + b + c), ans);
-                    minAns = Math.min(minAns, newAns);
-                    maxAns = Math.max(maxAns, newAns);
+        else if (n < 100) {
+            int tmp = n / 10 + n % 10;
+            solve(tmp, find_cnt(tmp) + ans);
+        }
+
+        else {
+            String s = String.valueOf(n);
+            for (int i=0; i< s.length()-2; i++) {
+                for (int j=i+1; j<s.length()-1; j++) {
+                    String a = s.substring(0, i+1);
+                    String b = s.substring(i+1, j+1);
+                    String c = s.substring(j+1);
+                    int now = Integer.parseInt(a) + Integer.parseInt(b) + Integer.parseInt(c);
+                    solve(now, ans + find_cnt(now));
                 }
             }
-        } else if (n.length() == 2) {
-            int a = Integer.parseInt(n.substring(0, 1));
-            int b = Integer.parseInt(n.substring(1));
-            int newAns = solve(String.valueOf(a + b), ans);
-            minAns = Math.min(minAns, newAns);
-            maxAns = Math.max(maxAns, newAns);
         }
 
-        return ans;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String a = br.readLine();
-        solve(a, 0);
-        minAns++;
+        int a = Integer.parseInt(br.readLine());
+        solve(a, find_cnt(a));
         System.out.println(minAns + " " + maxAns);
     }
 }
