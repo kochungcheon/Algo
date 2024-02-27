a15b24d442523cadf59f4e796a6000335e09443e
kochungcheon <g2c1010@naver.com>
2024-02-27 14:37:23 +0900

    [Bronze II] Title: Hashing, Time: 76 ms, Memory: 11532 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int len = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int idx = 1;
+        int ans = 0;
+        for (int i=0; i<len; i++) {
+            ans += (s.charAt(i)-'A'-31) * idx;
+            idx *= 31;
+        }
+        System.out.println(ans);
+
+    }
+}
