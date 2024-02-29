f73ab8d06f52c7653e66e402c8f194aa40a5b5a4
kochungcheon <g2c1010@naver.com>
2024-02-29 17:28:15 +0900

    [Bronze IV] Title: 폰 노이만과 파리, Time: 236 ms, Memory: 18688 KB -BaekjoonHub

---
+++ 백준/Bronze/14924. 폰 노이만과 파리/폰 노이만과 파리.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.out.println((arr[2]/(arr[0]*2)*arr[1]));
+    }
+}
