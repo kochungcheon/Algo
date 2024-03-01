6efab03adb0aaf5aea7c42e3f0561e190d79b37c
kochungcheon <g2c1010@naver.com>
2024-03-01 14:36:55 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer B = Integer.parseInt(br.readLine());
+        System.out.println(B * 100 / 110);
+    }
+}
