4147da99e134019ecd6882028cfca2db428e9461
kochungcheon <g2c1010@naver.com>
2024-03-22 17:20:29 +0900

    [Silver II] Title: 사탕 게임, Time: 336 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -1,72 +1,67 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         String[][] grid = new String[N][N];
-        
-        for (int i = 0; i < N; i++) {
+        for (int i=0; i<N; i++) {
             grid[i] = br.readLine().split("");
         }
-        
         int max = 0;
-        
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (j < N - 1) { // 오른쪽과 교환
-                    swap(grid, i, j, i, j + 1);
-                    max = Math.max(max, getMaxCount(grid, N));
-                    swap(grid, i, j, i, j + 1); // 원상 복구
+
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                if (j < N-1) {
+                    swap(grid, i, j, i, j+1);
+                    max = Math.max(max, getMaxCnt(grid, N));
+                    swap(grid, i, j, i, j+1);
                 }
-                
-                if (i < N - 1) { // 아래와 교환
-                    swap(grid, i, j, i + 1, j);
-                    max = Math.max(max, getMaxCount(grid, N));
-                    swap(grid, i, j, i + 1, j); // 원상 복구
+
+                if (i < N-1) {
+                    swap(grid, i, j, i+1, j);
+                    max = Math.max(max, getMaxCnt(grid, N));
+                    swap(grid, i, j, i+1, j);
                 }
             }
         }
-        
         System.out.println(max);
     }
-    
-    public static void swap(String[][] grid, int x1, int y1, int x2, int y2) {
-        String temp = grid[x1][y1];
+
+    static void swap(String[][] grid, int x1, int y1, int x2, int y2) {
+        String tmp = grid[x1][y1];
         grid[x1][y1] = grid[x2][y2];
-        grid[x2][y2] = temp;
+        grid[x2][y2] = tmp;
     }
-    
-    public static int getMaxCount(String[][] grid, int N) {
-        int maxCount = 1;
-        
-        for (int i = 0; i < N; i++) {
-            int rowCount = 1;
-            int colCount = 1;
-            for (int j = 1; j < N; j++) {
-                // 행 검사
-                if (grid[i][j].equals(grid[i][j - 1])) {
-                    rowCount++;
+
+    static int getMaxCnt(String[][] grid, int N) {
+        int tmpMax = 1;
+        for (int i=0; i<N; i++) {
+            int rawCnt = 1;
+            int calCnt = 1;
+            for (int j=0; j<N-1; j++) {
+                if (grid[i][j].equals(grid[i][j+1])) {
+                    rawCnt++;
                 } else {
-                    maxCount = Math.max(maxCount, rowCount);
-                    rowCount = 1;
+                    tmpMax = Math.max(tmpMax, rawCnt);
+                    rawCnt = 1;
                 }
-                
-                // 열 검사
-                if (grid[j][i].equals(grid[j - 1][i])) {
-                    colCount++;
+
+                if (grid[j][i].equals(grid[j+1][i])) {
+                    calCnt++;
                 } else {
-                    maxCount = Math.max(maxCount, colCount);
-                    colCount = 1;
+                    tmpMax = Math.max(tmpMax, calCnt);
+                    calCnt = 1;
                 }
             }
-            maxCount = Math.max(maxCount, Math.max(rowCount, colCount)); // 마지막 비교에서 최대값 업데이트
+            tmpMax = Math.max(tmpMax, Math.max(rawCnt, calCnt));
         }
-        
-        return maxCount;
+        return tmpMax;
     }
 }
