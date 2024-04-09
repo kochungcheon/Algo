88f5d71f952fd313f20d6abf9eae59b0aae9188a
kochungcheon <g2c1010@naver.com>
2024-04-09 21:57:26 +0900

    [Bronze II] Title: 그릇, Time: 76 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/7567. 그릇/그릇.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int ans = 10;
+        for (int i=1; i<s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i-1)) {
+                ans += 5;
+            } else {
+                ans += 10;
+            }
+        }
+        System.out.println(ans);
+    }
+}
