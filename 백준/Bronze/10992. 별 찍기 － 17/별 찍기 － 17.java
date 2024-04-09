b9a848ad40a8271e66a834dcd4af416239822bd7
kochungcheon <g2c1010@naver.com>
2024-04-09 17:27:22 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 300 ms, Memory: 16544 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
@@ -0,0 +1,23 @@
+import javax.lang.model.type.ArrayType;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<N; i++) {
+            for (int j=1; j<=N+i; j++) {
+                if (j == N - i || j == N + i || i == N - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
