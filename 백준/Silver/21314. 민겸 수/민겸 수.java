cbf970576159fc41ab88f58fed2f730f5076c05f
kochungcheon <g2c1010@naver.com>
2024-05-16 18:31:05 +0900

    [Silver I] Title: 민겸 수, Time: 80 ms, Memory: 11564 KB -BaekjoonHub

--- 백준/Silver/21314. 민겸 수/민겸 수.java
+++ 백준/Silver/21314. 민겸 수/민겸 수.java
@@ -1,69 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 	static String findMax(String target) {
 		StringBuilder sb = new StringBuilder();
 		int tmp = 0;
-		for (int i=0; i<target.length(); i++) {
-			char now = target.charAt(i);
-			if (now == 'K' && tmp == 0) {
+		for (char now : target.toCharArray()) {
+			if (now == 'K') {
 				sb.append(5);
+				while (tmp-- > 0) sb.append(0);
+				tmp = 0;
 			} else {
-				if (now == 'K') {
-					sb.append(5);
-					for (int j=0; j<tmp; j++) {
-						sb.append(0);
-					}
-					tmp = 0;
-				} else {
-					tmp ++;
-				}
+				tmp++;
 			}
 		}
-		if (tmp != 0) {
-			for (int j=0; j<tmp; j++) {
+		while (tmp-- > 0) sb.append(1);
+		return sb.toString();
+	}
+
+	static String findMin(String target) {
+		StringBuilder sb = new StringBuilder();
+		boolean startM = target.charAt(0) == 'M';
+		if (startM) sb.append(1);
+		
+		for (int i = 1; i < target.length(); i++) {
+			char now = target.charAt(i);
+			char before = target.charAt(i - 1);
+			if (now == 'K') {
+				sb.append(5);
+			} else if (before == 'M') {
+				sb.append(0);
+			} else {
 				sb.append(1);
 			}
 		}
+		if (!startM && target.charAt(0) == 'K') sb.insert(0, 5);
 		return sb.toString();
 	}
-	static String findMin(String target) {
-		// K는 5
-		// 뒤에 M 이 오면 0 맨 앞 M 은 1
-		StringBuilder sb = new StringBuilder();
-		char before = '\n';
-		for (int i=0; i<target.length(); i++) {
-			char now = target.charAt(i);
-			if (before == '\n') {
-				if (now == 'M') {
-					sb.append(1);
-				} else {
-					sb.append(5);
-				}
-			}
-			else {
-				if (now == 'K') {
-					sb.append(5);
-				}
-				else {
-					if (before == 'M') {
-						sb.append(0);
-					}
-					else {
-						sb.append(1);
-					}
-				}
-			}
-			before = now;
-		}
-		return sb.toString();
-	}
-	static String str;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		str = br.readLine();
+		String str = br.readLine();
 		System.out.println(findMax(str));
 		System.out.println(findMin(str));
 	}
