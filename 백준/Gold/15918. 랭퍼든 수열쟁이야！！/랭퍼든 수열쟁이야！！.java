a62abc0c1c78219661a71c5cbdda96b17d710994
kochungcheon <g2c1010@naver.com>
2024-05-20 15:29:09 +0900

    [Gold V] Title: 랭퍼든 수열쟁이야!!, Time: 260 ms, Memory: 12348 KB -BaekjoonHub

---
+++ 백준/Gold/15918. 랭퍼든 수열쟁이야！！/랭퍼든 수열쟁이야！！.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, X, Y, ans;
+	static int[] arr, visit;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		X = Integer.parseInt(st.nextToken());
+		Y = Integer.parseInt(st.nextToken());
+		ans = 0;
+
+		arr = new int[25];
+		visit = new int[25];
+
+		arr[X] = arr[Y] = Y - X - 1;
+		visit[Y-X-1] = 1;
+		check(1);
+		System.out.println(ans);
+	}
+
+	private static void check(int cur) {
+		if (cur == N*2+1) {
+			ans++;
+			return;
+		}
+		if (arr[cur] != 0) {
+			check(cur + 1);
+			return;
+		}
+
+		for (int i=1; i<=N; i++) {
+			if (visit[i] == 1) continue;
+			if (cur + i + 1 <= 2*N && arr[cur + i + 1] == 0) {
+				arr[cur] = arr[cur + i + 1] = i;
+				visit[i] = 1;
+				check(cur + 1);
+				arr[cur] = arr[cur + i + 1] = visit[i] = 0;
+			}
+		}
+	}
+}
