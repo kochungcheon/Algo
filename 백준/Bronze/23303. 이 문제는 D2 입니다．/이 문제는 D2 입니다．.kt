a4bc3e6aa34ccfbfe767e859dd1761c40d1ef04b
kochungcheon <g2c1010@naver.com>
2024-05-26 15:02:14 +0900

    [Bronze III] Title: 이 문제는 D2 입니다., Time: 128 ms, Memory: 16412 KB -BaekjoonHub

---
+++ 백준/Bronze/23303. 이 문제는 D2 입니다．/이 문제는 D2 입니다．.kt
@@ -0,0 +1,8 @@
+fun main() {
+        val s = readln();
+        if (s.contains("d2") || s.contains("D2")) {
+            println("D2")
+        } else {
+            println("unrated")
+        }
+    }
