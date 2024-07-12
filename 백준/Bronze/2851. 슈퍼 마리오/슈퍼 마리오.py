bd0591e7f4d6ee5d04c840a14cfb203467a336af
kochungcheon <g2c1010@naver.com>
2024-07-13 00:18:26 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 112 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
@@ -0,0 +1,17 @@
+score_list = []
+score = 0
+
+for _ in range(10):
+    score_list.append(int(input()))
+
+for i in range(10):
+    score += score_list[i]
+    if score > 100:
+        if abs(sum(score_list[:i])-100) < abs(sum(score_list[:i+1])-100):
+            print(sum(score_list[:i]))
+            break
+        else:
+            print(sum(score_list[:i+1]))
+            break
+else:
+    print(sum(score_list))
