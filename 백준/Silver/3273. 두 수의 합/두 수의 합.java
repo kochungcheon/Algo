fa10a793d36326888f8f213bad2dc6191a52d340
kochungcheon <g2c1010@naver.com>
2024-07-17 23:45:08 +0900

    [Silver III] Title: 두 수의 합, Time: 468 ms, Memory: 32908 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -4,26 +4,27 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        Arrays.sort(arr);
-        int X = Integer.parseInt(br.readLine());
-
-        int ans = 0;
-        for (int i=0; i<arr.length-1; i++) {
-            for (int j=i+1; j<arr.length; j++) {
-                int tmp = arr[i] + arr[j];
-                if (tmp == X) {
-                    ans++;
-                }
-                else if (tmp > X) {
-                    break;
-                }
-            }
-        }
-
-        System.out.println(ans);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		Arrays.sort(arr);
+		int purpose = Integer.parseInt(br.readLine());
+		int ans = 0;
+		int s = 0;
+		int e = N-1;
+		while (s < e) {
+			int now = arr[s] + arr[e];
+			if (now == purpose) {
+				s++;
+				e--;
+				ans++;
+			} else if (now > purpose) {
+				e--;
+			} else {
+				s++;
+			}
+		}
+		System.out.print(ans);
+	}
 }
