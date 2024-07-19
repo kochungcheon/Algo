229d8bfc9c477d017dd2696959b449b0a71065f9
kochungcheon <g2c1010@naver.com>
2024-07-19 23:07:41 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 692 ms, Memory: 142804 KB -BaekjoonHub

---
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+	    int s = 0;
+		int e = 0;
+
+		int ans = 0;
+		int life = K;
+		int tmp = 0;
+
+		while(e < N && s <= e) {
+			if (arr[e] % 2 == 1) {
+				life--;
+			} else {
+				tmp++;
+			}
+			ans = Math.max(ans, tmp);
+			if (life < 0) {
+				while (s <= e) {
+					if (arr[s++] % 2 == 1) {
+						life++;
+						break;
+					} else {
+						tmp--;
+					}
+				}
+			}
+			e++;
+		}
+
+		System.out.print(ans);
+	}
+}
