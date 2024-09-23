ac447e0603d6db4dc4f6f1b47ffcf3377870a287
kochungcheon <g2c1010@naver.com>
2024-09-24 00:04:26 +0900

    [Silver V] Title: 숫자 카드, Time: 2928 ms, Memory: 233488 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int N;
+    static int[] A;
+    static int M;
+    static int[] B;
+    static int find(int x) {
+        int s = 0;
+        int e = N-1;
+        while (e >= s) {
+            int mid = (s + e) / 2;
+            if (A[mid] == x) {
+                return 1;
+            }
+            if (A[mid] > x) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        A = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        M = Integer.parseInt(br.readLine());
+        B = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(A);
+        for (int b : B) {
+            System.out.print(find(b) + " ");
+        }
+    }
+}
