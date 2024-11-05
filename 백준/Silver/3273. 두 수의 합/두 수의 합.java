b336673c8fd690c0d6f00d2ed74b4dbd3355908c
kochungcheon <g2c1010@naver.com>
2024-11-05 22:55:26 +0900

    [Silver III] Title: 두 수의 합, Time: 368 ms, Memory: 32340 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -2,29 +2,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		Arrays.sort(arr);
-		int purpose = Integer.parseInt(br.readLine());
-		int ans = 0;
-		int s = 0;
-		int e = N-1;
-		while (s < e) {
-			int now = arr[s] + arr[e];
-			if (now == purpose) {
-				s++;
-				e--;
-				ans++;
-			} else if (now > purpose) {
-				e--;
-			} else {
-				s++;
-			}
-		}
-		System.out.print(ans);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                System.in
+        ));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr  = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int target = Integer.parseInt(br.readLine());
+
+        Arrays.sort(arr);
+
+        int s = 0;
+        int e = arr.length - 1;
+
+        int ans = 0;
+        while (s < e) {
+            int now = arr[s] + arr[e];
+            if (now == target) {
+                ans++;
+                s++;
+                e--;
+            }
+            else if (now > target) {
+                e--;
+            } else {
+                s++;
+            }
+        }
+        System.out.print(ans);
+    }
 }
