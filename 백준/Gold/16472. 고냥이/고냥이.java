9e7a2e09f5312ebd8461aa73cbbfb7f1c5034425
kochungcheon <g2c1010@naver.com>
2024-11-28 01:13:56 +0900

    [Gold IV] Title: 고냥이, Time: 100 ms, Memory: 14012 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -3,32 +3,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		String str = br.readLine();
-		int len = str.length();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String str = br.readLine();
 
-		int[] alpha = new int[26];
+        int[] arr = new int[26];
+        int cnt = 0;
+        int s = 0;
+        int e = 0;
+        int ans = 0;
 
-		int s = 0;
-		int e = 0;
-		int cnt = 0;
-		int ans = 0;
-		while (s <= e && e < len) {
-			char now = str.charAt(e);
-			if (++alpha[now - 'a'] == 1) cnt++;
-			while (cnt > N) {
-				char sNow = str.charAt(s);
-				s++;
-				if (--alpha[sNow-'a'] == 0) {
-					cnt--;
-					break;
-				}
-			}
-			ans = Math.max(ans, e - s + 1);
-			e++;
-		}
-		System.out.print(ans);
-	}
+        while (str.length() > e) {
+            int idx = str.charAt(e++) - 'a';
+            arr[idx]++;
+            if (arr[idx] == 1) {
+                cnt++;
+            }
+
+            if (cnt > N) {
+                while (cnt > N) {
+                    int tmp = str.charAt(s++) - 'a';
+                    arr[tmp]--;
+                    if (arr[tmp] == 0) {
+                        cnt--;
+                    }
+                }
+            }
+
+            ans = Math.max(ans, e - s);
+        }
+
+        System.out.print(ans);
+
+    }
 }
