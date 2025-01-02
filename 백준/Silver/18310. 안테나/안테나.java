b6bc8cb4658b770015ff4f60e739e031b04235c8
kochungcheon <g2c1010@naver.com>
2025-01-02 21:28:07 +0900

    [Silver III] Title: 안테나, Time: 540 ms, Memory: 50904 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+        System.out.print(arr[(N-1)/2]);
+    }
+}
