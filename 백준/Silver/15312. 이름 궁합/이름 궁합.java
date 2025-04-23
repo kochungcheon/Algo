7ea81ea7f12b8c819b0916eb26290a9d7c0faa75
kochungcheon <g2c1010@naver.com>
2025-04-23 17:52:14 +0900

    [Silver V] Title: 이름 궁합, Time: 124 ms, Memory: 42944 KB -BaekjoonHub

---
+++ 백준/Silver/15312. 이름 궁합/이름 궁합.java
@@ -0,0 +1,34 @@
+
+
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String he = br.readLine();
+		String her = br.readLine();
+
+		int len = he.length() * 2;
+		int[] alpha = new int[]{3, 2, 1, 2, 3, 3, 2, 3, 3, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1};
+
+		int[] now = new int[len];
+		int[] next = new int[len];
+
+ 		for (int i=0; i<len / 2; i++) {
+			now[i * 2] = alpha[he.charAt(i) - 'A'];
+			now[i * 2 + 1] = alpha[her.charAt(i) - 'A'];
+		}
+
+		while (len > 2) {
+			for (int i=0; i<len-1; i++) {
+				next[i] = (now[i] + now[i+1]) % 10;
+			}
+
+			now = next;
+			len--;
+			next = new int[len];
+		}
+
+		System.out.print("" + now[0] + now[1]);
+	}
+}
