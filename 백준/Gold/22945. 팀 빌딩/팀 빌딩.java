2b24da201a667e5eb8900ffac32595f89bf7b7bc
kochungcheon <g2c1010@naver.com>
2025-07-28 10:57:48 +0900

    [Gold IV] Title: 팀 빌딩, Time: 332 ms, Memory: 28944 KB -BaekjoonHub

--- 백준/Gold/22945. 팀 빌딩/팀 빌딩.java
+++ 백준/Gold/22945. 팀 빌딩/팀 빌딩.java
@@ -1,24 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	static int N;
+	static int[] arr;
+	static int ans;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		N = Integer.parseInt(br.readLine());
+		arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		ans = 0;
+		
 		int s = 0;
-		int e = N-1;
-		int ans = 0;
+		int e = N - 1;
 		while (s < e) {
-			ans = Math.max(ans, (e - s - 1) * Math.min(arr[s], arr[e]));
+			ans = cal(s, e);
 			if (arr[s] < arr[e]) s++;
 			else e--;
 		}
-		System.out.print(ans);
+        System.out.print(ans);
+	}
+	static int cal(int s, int e) {
+		return Math.max(ans, (e - s - 1) * Math.min(arr[s], arr[e]));
 	}
 }
