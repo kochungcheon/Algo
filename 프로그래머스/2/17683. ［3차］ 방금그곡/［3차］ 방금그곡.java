40aa46b1c2b873da8812b6e8a211f55fdc4fff68
kochungcheon <g2c1010@naver.com>
2025-09-10 19:50:52 +0900

    [level 2] Title: [3차] 방금그곡, Time: 21.15 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -1,30 +1,54 @@
 import java.util.*;
+import java.io.*;
+
 class Solution {
-	public static String solution(String m, String[] musicinfos) {
-		String answer = "(None)";
-		int maxTime = 0;
+    public String solution(String m, String[] musicinfos) {
+        String answer = "(None)";
+        int len = musicinfos.length;
+        // System.out.println(len);
+        String target = convert(m);
+        // 재생 시간
+        int maxTime = -1;
+        for (int i=0; i<len; i++) {
+            String[] musicinfo = musicinfos[i].split(",");
+            int startTime = convertTime(musicinfo[0].split(":"));
+            int endTime = convertTime(musicinfo[1].split(":"));
+            int time = converTimeTerm(startTime, endTime);
+            
+            String title = musicinfo[2];
+            String melody = convertMusic(convert(musicinfo[3]), time);
 
-		m = m.replace("C#", "c").replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#", "a");
-
-		for (String musicinfo : musicinfos) {
-			String[] info = musicinfo.split(",");
-			String[] start = info[0].split(":");
-			String[] end = info[1].split(":");
-			int time = (Integer.parseInt(end[0]) - Integer.parseInt(start[0])) * 60 + Integer.parseInt(end[1]) - Integer.parseInt(start[1]);
-
-			info[3] = info[3].replace("C#", "c").replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#", "a");
-
-			String melody = "";
-			for (int i = 0; i < time; i++) {
-				melody += info[3].charAt(i % info[3].length());
-			}
-
-			if (melody.contains(m) && time > maxTime) {
-				maxTime = time;
-				answer = info[2];
-			}
-		}
-
-		return answer;
-	}
+            // System.out.println(melody);
+            if (melody.contains(target) && time > maxTime) {
+                maxTime = time;
+                answer = title;
+            }
+            
+            
+        }
+        return answer;
+    }
+    public String convert(String m) {
+        return m.replaceAll("C#", "H").replaceAll("D#", "I").replaceAll("F#", "J").replaceAll("G#", "K").replaceAll("A#", "L").replaceAll("B#", "1").replaceAll("E#", "2");
+    }
+    
+    public String convertMusic(String music, int time) {
+        StringBuilder sb = new StringBuilder();
+        int len = music.length();
+        for (int i=0; i<time; i++) {
+            sb.append(music.charAt(i % len));
+        }
+        // System.out.println(sb);
+        return sb.toString();
+    }
+    
+    public int convertTime(String[] time) {
+        int hour = Integer.parseInt(time[0]) * 60;
+        int min = Integer.parseInt(time[1]);
+        return hour + min;
+    }
+    
+    public int converTimeTerm(int startTime, int endTime) {
+        return endTime - startTime;
+    }
 }
