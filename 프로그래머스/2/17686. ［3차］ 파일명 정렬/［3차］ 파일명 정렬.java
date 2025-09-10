5d03106e6854188b4cb818128901f1b9d3cfdedc
kochungcheon <g2c1010@naver.com>
2025-09-10 17:02:45 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 15.39 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,55 +1,60 @@
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.*;
 
 class Solution {
-    private static String[] splitFileName(String fileName) {
-        Pattern pattern = 
-            Pattern.compile("([a-zA-Z,\\- ]+)(\\d+)(.*)");
-        Matcher matcher = pattern.matcher(fileName);
+    static class File {
+        String head;
+        int number;
+        int index;
         
-        if (matcher.matches()) {
-            return new String[]{matcher.group(1), matcher.group(2), matcher.group(3)};
-        } else {
-            return null;
+        public File(String head, int number, int index) {
+            this.head = head.toLowerCase();
+            this.number = number;
+            this.index = index;
+        }
+        
+        public String getHead() {
+            return this.head;
+        }
+        public int getNumber() {
+            return this.number;
+        }
+        public int getIndex() {
+            return this.index;
         }
     }
-
-    static class Pair {
-        String HEAD;
-        int NUMBER;
-        int INDEX;
-
-        public Pair(String HEAD, int NUMBER, int INDEX) {
-            this.HEAD = HEAD;
-            this.NUMBER = NUMBER;
-            this.INDEX = INDEX;
-        }
-    }
-
     public String[] solution(String[] files) {
-        Pair[] pairs = new Pair[files.length];
-        for (int i = 0; i < files.length; i++) {
-            String[] parts = splitFileName(files[i]);
-            pairs[i] = new Pair(parts[0].toLowerCase(), Integer.parseInt(parts[1]), i);
-        }
-
-        Arrays.sort(pairs, new Comparator<Pair>() {
-            @Override
-            public int compare(Pair p1, Pair p2) {
-                int headCompare = p1.HEAD.compareTo(p2.HEAD);
-                if (headCompare != 0) return headCompare;
-                int numberCompare = Integer.compare(p1.NUMBER, p2.NUMBER);
-                if (numberCompare != 0) return numberCompare;
-                return Integer.compare(p1.INDEX, p2.INDEX);
+        int len = files.length;
+        String[] answer = new String[len];
+        PriorityQueue<File> pq = new PriorityQueue(
+            Comparator.comparing(File::getHead).thenComparing(File::getNumber).thenComparing(File::getIndex)
+        );
+        for (int i=0; i<len; i++) {
+            String file = files[i];
+            
+            int j = 0;
+            while (j < file.length() && !Character.isDigit(file.charAt(j))) {
+                j++;
             }
-        });
-
-        String[] answer = new String[files.length];
-        for (int i = 0; i < files.length; i++) {
-            answer[i] = files[pairs[i].INDEX];
+            
+            int k = j;
+            while (k < file.length() && Character.isDigit(file.charAt(k))) {
+                k++;
+            }
+            pq.add(
+            new File(
+                file.substring(0, j),
+                Integer.valueOf(file.substring(j, k)),
+                i
+            ) 
+            );
         }
-
+        int index = 0;
+        while (!pq.isEmpty()) {
+            File f = pq.poll();
+            answer[index++] = files[f.getIndex()];
+        }
+        
         return answer;
     }
 }
