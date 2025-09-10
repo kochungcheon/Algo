f29eedab3f59f499f3bdcbe3a2f29205293043df
kochungcheon <g2c1010@naver.com>
2025-09-10 16:18:10 +0900

    [level 2] Title: [3차] 압축, Time: 9.54 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,43 +1,53 @@
 import java.util.*;
-import java.io.*;
 
-// 65 ~ 90
-// 1 ~ 26
 class Solution {
     public int[] solution(String msg) {
+       
         int len = msg.length();
-
-        List<Integer> lst = new ArrayList<>();
-        Map<String, Integer> dict = new HashMap<>();
-
-        for (char c = 'A'; c <= 'Z'; c++) {
-            dict.put(String.valueOf(c), c - 'A' + 1);
+        String[] msg_lst = new String[len];
+        for (int i=0; i<len; i++){
+            msg_lst[i] = msg.substring(i, i+1);
         }
-        int nextIdx = 27;
-
-        for (int i = 0; i < len; i++) {
-            System.out.println(i);
-            int index = i + 1;
-
-            while (index < len && dict.containsKey(msg.substring(i, index + 1))) {
-                index++;
-            }
-
-            String w = msg.substring(i, index);
-            lst.add(dict.get(w));
-            
-            if (index < len) {
-                String str = msg.substring(i, index + 1);
-                if (!dict.containsKey(str)) {
-                    dict.put(str, nextIdx++);
+        HashMap<String, Integer> map = new HashMap<>();
+        // 초기 값 할당
+        int index = 1;
+        for (char i='a'; i<= 'z'; i++) {
+            map.put(Character.toString(i).toUpperCase(), index++);
+        }
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        
+        for (int i=0; i<len; i++) {
+            String tmp = msg_lst[i];
+            for (int j=i; j<len; j++) {
+                // 만약 키가 있다면
+                if (map.containsKey(tmp)) {
+                    // 여유가 있으면 
+                    if (j+1 < len){
+                        tmp += msg_lst[j+1];
+                    } else {
+                        System.out.println(tmp);
+                        arr.add(map.get(tmp));
+                        System.out.println(map.get(tmp));
+                        i = j;
+                    }
+                    // 0
+                    //     0 1
+                } else {
+                    // System.out.println(" i : " + i + " j : " + j + " " + " tmp : " + tmp);
+                    arr.add(map.get(tmp.substring(0, tmp.length()-1)));
+                    // System.out.println(map.get(tmp.substring(0, tmp.length()-1)));
+                    i = j-1;
+                    map.put(tmp, index++);
+                    break;
                 }
+                
             }
-            
-            i = index - 1;
+    
         }
-
-        int[] answer = new int[lst.size()];
-        for (int k = 0; k < lst.size(); k++) answer[k] = lst.get(k);
+        int[] answer = new int[arr.size()];
+        for (int i=0; i<arr.size(); i++) {
+            answer[i] = arr.get(i);
+        }    
         return answer;
     }
 }
