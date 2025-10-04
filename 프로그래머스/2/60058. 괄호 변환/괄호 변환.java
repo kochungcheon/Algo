1807281a30bf0ca88e8565c8dc38ba4502c6d5f6
kochungcheon <g2c1010@naver.com>
2025-10-04 20:36:11 +0900

    [level 2] Title: 괄호 변환, Time: 10.44 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -1,57 +1,51 @@
-import java.util.*;	
-class Solution {
-		static int index;
-		static boolean check(String p) {
-			Stack<Character> stack = new Stack<>();
-			boolean flag = true;
-			int open = 0;
-			int close = 0;
-			for (int i=0; i<p.length(); i++) {
-				if (p.charAt(i) == '(') {
-					open++;
-					stack.push('(');
-				} else {
-					close++;
-					if (stack.isEmpty()) {
-						flag = false;
-					} else {
-						stack.pop();
-					}
-				}
-				if (open == close) {
-					index = i + 1;
-					return flag;
-				}
-			}
-			return true;
-		}
-		public String solution(String p) {
-			if (p.isEmpty()) {
-				return p;
-			}
+import java.util.*;
+import java.io.*;
 
-			boolean flag = check(p);
-			String u = p.substring(0, index);
-			String v = p.substring(index);
-			// 3
-			if (flag) {
-				return u + solution(v);
-				// 4
-			} else {
-				String answer = "(";
-				
-				answer += solution(v);
-				
-				answer += ')';
-				
-				for (int i=1; i<u.length()-1; i++) {
-					if (u.charAt(i) == '(') {
-						answer += ")";
-					} else {
-						answer += "(";
-					}
-				}
-				return answer;
-			}
-		}
-	}
+class Solution {
+    public String transform(String w) {
+        if (w.isEmpty()) return "";
+        int cut = firstCut(w);
+        String u = w.substring(0, cut + 1);
+        String v = w.substring(cut + 1);
+        
+        if (isValid(u)) {
+            return u + transform(v);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append('(');
+        sb.append(transform(v));
+        sb.append(')');
+        
+        for (char c : u.substring(1, u.length() - 1).toCharArray()) {
+            sb.append(c == '(' ? ')' : '(');
+        }
+        return sb.toString();
+    }
+    public boolean isValid(String s) {
+        int bal = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') bal++;
+            else {
+                bal--;
+                if (bal < 0) return false;
+                };
+        }
+        return bal == 0;
+    }
+    public int firstCut(String w) {
+        int bal = 0;
+        for (int i=0; i<w.length(); i++) {
+            bal += (w.charAt(i) == '(' ? 1 : -1);
+            if (bal == 0) return i;
+        }
+        return 0;
+    }
+    public String solution(String p) {
+        String answer = transform(p);
+        return answer;
+    }
+}
+// 괄호 개수는 맞지만 짝이 맞지 않는 다
+// 모든 괄호 뽑아 올바른 순서로
+// 
