55fea4385072d7507f3a943ad5fe1020e5a49d06
kochungcheon <g2c1010@naver.com>
2025-10-04 20:36:27 +0900

    [level 2] Title: 괄호 변환, Time: 10.44 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -2,47 +2,9 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    public String transform(String w) {
-        if (w.isEmpty()) return "";
-        int cut = firstCut(w);
-        String u = w.substring(0, cut + 1);
-        String v = w.substring(cut + 1);
-        
-        if (isValid(u)) {
-            return u + transform(v);
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        sb.append('(');
-        sb.append(transform(v));
-        sb.append(')');
-        
-        for (char c : u.substring(1, u.length() - 1).toCharArray()) {
-            sb.append(c == '(' ? ')' : '(');
-        }
-        return sb.toString();
-    }
-    public boolean isValid(String s) {
-        int bal = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '(') bal++;
-            else {
-                bal--;
-                if (bal < 0) return false;
-                };
-        }
-        return bal == 0;
-    }
-    public int firstCut(String w) {
-        int bal = 0;
-        for (int i=0; i<w.length(); i++) {
-            bal += (w.charAt(i) == '(' ? 1 : -1);
-            if (bal == 0) return i;
-        }
-        return 0;
-    }
     public String solution(String p) {
-        String answer = transform(p);
+        if (p.length() <= 1) return "";
+        String answer = "";
         return answer;
     }
 }
