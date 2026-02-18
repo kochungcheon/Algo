ebe47b2fe3e6caa989b810cd893ca5fd802fe53e
kochungcheon <g2c1010@naver.com>
2026-02-18 22:54:16 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,6 +1,13 @@
-import java.util.*;
 class Solution {
-    public int solution(int[] numbers) {
-        return 45 - Arrays.stream(numbers).sum();
+    public int solution(int[] nums) {
+        int answer = 0;
+        int[] arr = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        for (int n : nums) {
+            arr[n]--;
+        }
+        for (int i=1; i<10; i++) {
+            if (arr[i] == 0) answer += i;
+        }
+        return answer;
     }
 }
