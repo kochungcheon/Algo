abce872ef6910b11b842ebf23f9b7c010d3ce876
kochungcheon <g2c1010@naver.com>
2026-03-09 09:53:33 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 181.82 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,22 +1,36 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] elements) {
-        int answer = 0;
         int len = elements.length;
-        boolean[] dp = new boolean[1000001];
-        for (int i=0; i<len; i++){
-            int tmp  = 0;
-            for (int j=0; j<len; j++){
-                tmp += elements[(i+j)%len];
-                // System.out.println(tmp);
-                dp[tmp] = true; 
+        int[] prefix = new int[len * 2 + 1];
+        // setting
+        for (int i=0; i<len; i++) {
+            prefix[i + 1] = elements[i];
+            prefix[i + 1 + len] = elements[i];
+        }
+        
+        for (int i=0; i< len * 2; i++) {
+            prefix[i + 1] += prefix[i]; 
+        }
+        
+        // check
+        // for (int i=0; i<= len * 2; i++) {
+        //     System.out.print(prefix[i] + " ");
+        // }
+        System.out.println();
+        HashSet<Integer> set = new HashSet<>();
+        for (int i=0; i<=len; i++) {
+            for (int j=i+1; j<=i + len; j++) {
+                int a = prefix[j] - prefix[i];
+                set.add(a);
             }
         }
-        for (int i=0; i<1000001; i++){
-            if (dp[i]){
-                answer++;
-            }
-        }
+        // for (int s : set) {
+        //     System.out.print(s + " ");
+        // }
+        
+        int answer = set.size();
         return answer;
     }
 }
