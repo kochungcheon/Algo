a0fe985a53f81815ee9f62ebc0a7b3cf17916c76
kochungcheon <g2c1010@naver.com>
2026-03-13 22:21:51 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1096 ms, Memory: 195236 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,66 +1,30 @@
-import java.io.*;
 import java.util.*;
-public class Main {
-    static int N;
-    static int[] A;
-    static int M;
-    static int[] B;
-    static int findMin(int x) {
-        int s = 0;
-        int e = N - 1;
-        int val = N;
-        while (e >= s) {
-            int mid = (s + e) / 2;
+import java.io.*;
 
-            if (A[mid] == x) {
-                val = Math.min(val, mid);
-                e = mid - 1;
-                continue;
-            }
-            if (A[mid] > x) {
-                e = mid - 1;
-            } else {
-                s = mid + 1;
-            }
-        }
-        return val;
-    }
-    static int findMax(int x) {
-        int s = 0;
-        int e = N - 1;
-        int val = -1;
-        while (e >= s) {
-            int mid = (s + e) / 2;
-            if (A[mid] == x) {
-                val = Math.max(val, mid);
-                s = mid + 1;
-                continue;
-            }
-            if (A[mid] >= x) {
-                e = mid - 1;
-            } else {
-                s = mid + 1;
-            }
-        }
-        return val;
-    }
+// 숫자 카드 2 (백준 10816)
+public class Main {
+    static int N, M;
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         N = Integer.parseInt(br.readLine());
-        A = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        Arrays.sort(A);
-        M = Integer.parseInt(br.readLine());
-        B = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        StringBuilder sb = new StringBuilder();
-        for (int b : B) {
-            int maxx = findMax(b);
-            int minx = findMin(b);
-            if (maxx < 0) {
-                sb.append(0).append(" ");
-            } else {
-                sb.append(maxx + 1 - minx).append(" ");
-            }
+        int[] cards = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<N; i++) {
+            map.put(cards[i], map.getOrDefault(cards[i], 0) + 1);
         }
-        System.out.print(sb);
+
+        M = Integer.parseInt(br.readLine());
+        int[] target = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        StringBuilder sb = new StringBuilder();
+        for (int t : target) {
+            sb.append(map.getOrDefault(t, 0)).append(" ");
+        }
+        System.out.println(sb);
+
     }
 }
