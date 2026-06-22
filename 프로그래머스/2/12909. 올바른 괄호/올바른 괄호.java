4e5486d4294bb9ab5770d2591564075d47a7945e
kochungcheon <g2c1010@naver.com>
2026-06-23 00:12:25 +0900

    [level 2] Title: 올바른 괄호, Time: 7.97 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,27 +1,27 @@
 import java.util.*;
-import java.lang.*;
+
 class Solution {
-    
     boolean solution(String s) {
         boolean answer = true;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         Deque<Character> stack = new ArrayDeque<>();
-        for (int i=0; i<s.length(); i++){
-            // System.out.println(answer);
+        for (int i=0; i<s.length(); i++) {
+            // 
             char c = s.charAt(i);
-            if (c == '('){
-                stack.push(c);
-            } else{
-                if (stack.isEmpty() || stack.peek() == ')'){
-                    answer = false;
+            if (c == '(') {
+                stack.add(c);
+            }
+            else {
+                if (stack.size() >= 1 && stack.peek() == '(') {
+                    stack.pop();
                 } else {
-                     stack.pop();
+                    stack.add(c);
                 }
             }
         }
-        if (!stack.isEmpty()){
-                answer = false;
-            }
-            
+        if (stack.size() > 0) return false;
+
         return answer;
     }
 }
