46fa2f2ec6bc0df311e22992473da5ed5a641c80
kochungcheon <g2c1010@naver.com>
2026-06-23 00:20:05 +0900

    [level 2] Title: 최솟값 만들기, Time: 7.17 ms, Memory: 57.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int len = A.length;
+        for (int i=0; i<len; i++) {
+            answer += A[i] * B[len - i - 1];
+        }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
